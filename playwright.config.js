c7c65ca7b42869f7afbfd07eb62659ab39d67eab
Ed hominem <derryd.derajat@gmail.com>
2025-10-31 19:34:23 +0700

    Add metadata to Playwright configuration

--- playwright.config.js
+++ playwright.config.js
@@ -11,5 +11,9 @@ module.exports = {
     },
     screenshot:'on'
   },
+   metadata: {
+    "project": process.env.GITHUB_REPOSITORY || "local",
+    "build": process.env.GITHUB_RUN_NUMBER || Date.now().toString()
+  },
   reporter: [ ['allure-playwright']],
 };
